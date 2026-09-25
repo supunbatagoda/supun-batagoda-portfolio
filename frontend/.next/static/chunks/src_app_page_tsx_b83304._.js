2c9ab11194ea71346b20b02e4e82bd545df95122
smb <supunmbatagoda@gmail.com>
2026-09-25 13:50:38 +0530

    Fix git workflow ssh issue

---
+++ frontend/.next/static/chunks/src_app_page_tsx_b83304._.js
@@ -0,0 +1,11 @@
+(globalThis.TURBOPACK = globalThis.TURBOPACK || []).push([
+    "static/chunks/src_app_page_tsx_b83304._.js",
+    {},
+]);
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+  "path": "static/chunks/src_app_page_tsx_b83304._.js",
+  "chunks": [
+    "static/chunks/_b2e9d4._.js"
+  ],
+  "source": "dynamic"
+});
--- frontend/.next/types/app/layout.ts
+++ frontend/.next/types/app/layout.ts
@@ -1,4 +1,4 @@
-// File: /Users/supunbatagoda/dev/supun-batagoda-portfolio/frontend/src/app/layout.tsx
+// File: /home/supunbatagoda/dev/supun-batagoda-portfolio/frontend/src/app/layout.tsx
 import * as entry from '../../../src/app/layout.js'
 import type { ResolvingMetadata, ResolvingViewport } from 'next/dist/lib/metadata/types/metadata-interface.js'
 
